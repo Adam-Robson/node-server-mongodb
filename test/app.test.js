bc55ff6c1d52462717ed58f8c4eae2a4f625f3ca
Adam Robson <adamrayrobson@gmail.com>
2024-08-02 01:44:08 -0700

    fix use of app object in test

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,22 @@
 // test/app.test.js
 import assert from 'assert';
 import http from 'http';
-import app from '../src/lib/app'; 
+import app from '../src/lib/app.js'; 
 
 describe('API Tests', function() {
-  it('should return 200 for the GET / route', function(done) {
+  let server;
 
+  // Start the server before running the tests
+  before((done) => {
+    server = app.listen(3000, done);
+  });
+
+  // Stop the server after running the tests
+  after((done) => {
+    server.close(done);
+  });
+
+  it('should return 200 for the GET / route', function(done) {
     const options = {
       hostname: 'localhost',
       port: 3000,
@@ -14,16 +25,8 @@ describe('API Tests', function() {
     };
 
     const req = http.request(options, (res) => {
-      let data = '';
-
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      res.on('end', () => {
-        assert.strictEqual(res.statusCode, 200);
-        done();
-      });
+      assert.strictEqual(res.statusCode, 200);
+      done();
     });
 
     req.on('error', (e) => {
